fe1c70f9543ea34ecdc3965c051f50742a384719
Hannah Plowes <hannahplowes958@gmail.com>
2025-05-30 13:36:13 +1000

    initial commit

---
+++ build.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "flux",
+        .root_module = exe_mod,
+    });
+
+    b.installArtifact(exe);
+}
---
+++ src/main.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("haiiii :3\n", .{});
+}
