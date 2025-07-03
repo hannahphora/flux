29a5a6391a70c167a26028fd611873b26e4d2a25
Hannah Plowes <hannahplowes@proton.me>
2025-07-03 15:55:11 +1000

    remove vscode folder

---
+++ src/renderer/internal/vk_assorted.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <renderer/renderer.h>
+
+namespace vk {
+//--------------------------------------------------------------------------------------------
+
+bool checkValidationLayerSupport() {
+    u32 layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+        for (const VkLayerProperties& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+        if (!layerFound)
+            return false;
+    }
+    return true;
+}
+
+//--------------------------------------------------------------------------------------------
+};
